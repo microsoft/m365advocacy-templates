257d7bf422b1fba20348d6eca0e71bab8d923c56
Garry Trinder <garry@trinder365.co.uk>
2024-11-28 13:54:38 +0000

    Reorder properties in ConfigOptions and update teamsapp.local.yml for consistency

--- templates/dotnet-teams/content/custom-engine-agent/Custom.Engine.Agent/Config.cs
+++ templates/dotnet-teams/content/custom-engine-agent/Custom.Engine.Agent/Config.cs
@@ -2,11 +2,11 @@ namespace Custom.Engine.Agent;
 
 public class ConfigOptions
 {
+    public string AZURE_OPENAI_DEPLOYMENT_NAME { get; set; }
+    public string AZURE_OPENAI_ENDPOINT { get; set; }
+    public string AZURE_OPENAI_KEY { get; set; }
+    public string AZURE_STORAGE_BLOB_CONTAINER_NAME { get; set; }
+    public string AZURE_STORAGE_CONNECTION_STRING { get; set; }
     public string BOT_ID { get; set; }
     public string BOT_PASSWORD { get; set; }
-    public string AZURE_OPENAI_KEY { get; set; }
-    public string AZURE_OPENAI_ENDPOINT { get; set; }
-    public string AZURE_OPENAI_DEPLOYMENT_NAME { get; set; }
-    public string AZURE_STORAGE_CONNECTION_STRING { get; set; }
-    public string AZURE_STORAGE_BLOB_CONTAINER_NAME { get; set; }
 }
